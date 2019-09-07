6fa066f7edba4e01d03a3e947450934fea35f4a0
Monique Chetty <sales@bluestormtrading.co.za>
2019-09-07 13:04:58 +0200

    first commit

---
+++ 06-ObjectsClasses_Practice1/src/soccer/Team.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package soccer;
+
+/**
+ *
+ * @author Sales
+ */
+public class Team {
+    public String teamName;
+    public Player[] playerArray;
+}
